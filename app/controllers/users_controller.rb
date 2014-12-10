eeae05c53ebc4c88f1f5bee4952e9248ebc042db
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-12-10 16:33:23 -0500

    added methods to the new and create actions in the user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
 
 def new
+	@user = User.new
 end
 
 def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_url, notice: "Signed up!"
+    else
+      render "new"
+    end
 end
 
 def show
@@ -17,6 +24,11 @@ end
 
 def destroy
 end
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 
 
 end
