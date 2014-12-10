2e838ca09576742afe9188df1c4c8cbe79d0636d
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-09 20:48:05 -0500

    Added inverse_of option to tradeable_cards and wanted_cards

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User
   include Mongoid::Document
+  include Mongoid::Attributes::Dynamic
 
-  has_and_belongs_to_many :tradeable_cards, class_name: 'Card'
-  has_and_belongs_to_many :wanted_cards, class_name: 'Card' #type: Hash?  
-  # has_many :cards, as: :binder #type: Hash?  
+  has_and_belongs_to_many :tradeable_cards, class_name: 'Card', inverse_of: :tradeable_by
+  has_and_belongs_to_many :wanted_cards, class_name: 'Card', inverse_of: :wanted_by    
   
   field :first_name, type: String
   field :last_name,  type: String
   field :email,      type: String
   field :dci_number, type: Integer
-  #field :tradeable_cards, type: Array
+
 end
