a763760a77b616e36e2f9c11baa2da2d44e40a02
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-12-10 16:20:33 -0500

    added password digest field and has_secure_password to user model (re: bcrypt functionality)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
 
+  has_secure_password
+
   has_and_belongs_to_many :tradeable_cards, class_name: 'Card', inverse_of: :tradeable_by
   has_and_belongs_to_many :wanted_cards, class_name: 'Card', inverse_of: :wanted_by    
   
@@ -9,5 +11,6 @@ class User
   field :last_name,  type: String
   field :email,      type: String
   field :dci_number, type: Integer
+  field :password_digest, type: String
 
 end
