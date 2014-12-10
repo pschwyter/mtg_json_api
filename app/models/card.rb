3de911b63884d43cc9dee76360ccc156b5f77cef
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-09 20:49:24 -0500

    Namespaced User into tradeable_by and wanted_by, and specified inverse_of option

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,16 +2,16 @@ class Card
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
 
-  has_and_belongs_to_many :users
+  belongs_to :card_set
+  has_and_belongs_to_many :tradeable_by, class_name: 'User', inverse_of: :tradeable_cards
+  has_and_belongs_to_many :wanted_by, class_name: 'User', inverse_of: :wanted_cards
 
-  # field :card, type: array
-  # Don't think I need to specify fields, bra
-  # field :layout, type: string
-  # field :type, type: string
-  # field :types, type: array
-  # field :layout, type: string
-  # field :layout, type: string
-  # field :layout, type: string
-  # field :layout, type: string
+  # In order for a user to place multiple "copies" of a card into either list
+  # add a new attribute to card, ie. "copies_by_user_ids". 
+  # card['tradeable_copies_by_user_ids'] = []; card.save
+  # card['wanted_copies_by_user_ids'] = []; card.save
+
+  # We then push the user.id to this array a number of times equal to the copies
+  # of that card in that list (tradeable_cards / wanted_cards)
 
 end
