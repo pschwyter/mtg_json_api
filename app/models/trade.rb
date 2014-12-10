5346e09802a770604b081a8545c1f1ef81fb0464
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-09 21:27:55 -0500

    Fixed typo in Trade model and added default values to cards arrays

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,14 +2,14 @@ class Trade
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
 
-  belongs_to :initatior, class_name: 'User'
+  belongs_to :initiator, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
 
   # has_many :initiator_cards, class_name: 'Card'
   # has_many :receiver_cards, class_name: 'Card'
 
-  field :initiator_cards, type: Array
-  field :receiver_cards, type: Array
+  field :initiator_cards, type: Array, default: []
+  field :receiver_cards, type: Array, default: []
 
   # {
   # 	trade: [
