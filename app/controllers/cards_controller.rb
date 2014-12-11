9384b7bf26de0321ad9f16d0952aece7b932acc0
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-11 13:11:19 -0500

    Rudimentary search function working

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,18 +6,34 @@ class CardsController < ApplicationController
 
   def index
 
-    if search_params
+    if params[:card_fields] #params[:card_fields]
+      search_params
+      query = Card.all
 
+      unless params[:card_fields][:name].blank?
+        query = query.where(name: Regexp.new(params[:card_fields][:name], 'i'))
+      end
+      unless params[:card_fields][:type].blank?
+        query = query.where(type: Regexp.new(params[:card_fields][:type], 'i'))
+      end
+      unless params[:card_fields][:subtypes].blank?
+        query = query.where(subtypes: Regexp.new(params[:card_fields][:subtypes], 'i'))
+      end
+      # unless params[:card_fields][:cmc].blank?
+      #   query = query.where(cmc: Regexp.new(params[:card_fields][:cmc], 'i'))
+      # end
+      @cards = query
+      # binding.pry
     else
-      @cards = Card.all
+      @cards = Card.where(name: "Black Lotus")
     end
 
   end
 
-  private 
+  private
 
   def search_params
-
+    params.require(:card_fields).permit(:name, :type, :subtypes, :cmc)
   end
 
 end
