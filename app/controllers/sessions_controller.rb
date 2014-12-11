cdbb0f4bb3c2e71b62be5249661de6ba6845292f
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-12-11 13:48:52 -0500

    added log in form to sessios/new view. Added create and destroy methods for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
+  	user = User.find_by(email: params[:email])
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		redirect_to new_user_url, notice: "Logged in!"
+  	else
+  		render "new"
+  	end
   end
 
   def destroy
+  	session[:user_id] = nil
+  	redirect_to new_user_url, notice: "Logged out!"
   end
+
 end
