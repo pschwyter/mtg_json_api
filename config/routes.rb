c1c55dc6de3c2ef22b40dc294e643e7de15a88aa
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-12-11 13:36:06 -0500

    added sessions controller and corresopnding actions/routes (new create destroy)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+
+
   resources :cards
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
