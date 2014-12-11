f60dcbc3d982fac9f7f0f6d97bd6f250acaa21d9
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-12-11 13:53:48 -0500

    added flash notice for unsucessful login to sessions controller. Also added flash method in layouts/application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
   		session[:user_id] = user.id
   		redirect_to new_user_url, notice: "Logged in!"
   	else
+  		flash.now[:alert] = "Invalid email or password"
   		render "new"
   	end
   end
