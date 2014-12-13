881a843cda40fa0a615b88d6618702e7d8806db4
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-12-13 13:08:48 -0500

    uncommented address in the user model...requird for geocode coordinates....i think.

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ has_secure_password
   field :dci_number, type: Integer
   field :password_digest, type: String
   field :coordinates, type: Array
-  # field :address, type: String #or is it an array? 
+  field :address, type: String #or is it an array? 
   # field :location_history 
   # def coordinates; location_history ? location_history.last : []; end
 
