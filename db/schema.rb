62ecd3a1b0f79ff3438827001196085a04054671
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-13 23:27:21 -0500

    Completed initial sorcery setup for User model and config

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141214033209) do
+ActiveRecord::Schema.define(version: 20141214042315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,5 +77,24 @@ ActiveRecord::Schema.define(version: 20141214033209) do
   create_table "subtypes", force: true do |t|
     t.string "name"
   end
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "dci_number"
+    t.string   "crypted_password",                null: false
+    t.string   "salt",                            null: false
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.string   "email",                           null: false
+  end
+
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
 
 end
