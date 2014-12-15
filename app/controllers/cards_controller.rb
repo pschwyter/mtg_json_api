6eec2a2dffd1576e15c17914b776b5a4819c88fa
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-14 20:01:53 -0500

    Search is functioning with card name, type, subtypes, colors, cmc and artist

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -18,42 +18,72 @@ class CardsController < ApplicationController
   def search
     search_params
     sanitized_search = search_params.delete_if { |k,v| v.blank? }
-    # binding.pry
     query = Card.all
-
     sanitized_search.each do |key, value|
+
+      if key == "name"
+        query = query.where(["#{key} iLIKE ?", "%#{value}%"])
+      end
       
       if key == "cmc"
         num = value.to_i
-        cmc = case sanitized_search[:cmcmod]
-          when "e" then :cmc
-          when "gt" then :cmc.gt
-          when "lt" then :cmc.lt
-          when "gte" then :cmc.gte
-          when "lte" then :cmc.lte
+        cmcmod = sanitized_search[:cmcmod]
+        query = query.where("#{key} #{cmcmod} ?", num)
+      end
+
+      if key == "card_type"
+        card_type_query = CardType.where(["name iLIKE ?", "%#{value}%"]).first.cards
+        query = [query, card_type_query].inject(&:&)
+      end
+
+      if key == "subtypes"
+        values = value.gsub(/\s+/, "").split(",")
+        subtype_query = []
+        values.each do |subtype|
+          subtype_query = Subtype.where(["name iLIKE ?", "%#{subtype}%"]).first.cards
+          query = [query, subtype_query].inject(&:&)
         end
+      end
 
-        query = query.where(cmc => num)
+      if key == "white" && value == "1"
+        color_query = Color.where(name: "White").first.cards
+        query = [query, color_query].inject(&:&)
+      end
 
-      elsif key == "cmcmod"
-        query = query
+      if key == "blue" && value == "1"
+        color_query = Color.where(name: "Blue").first.cards
+        query = [query, color_query].inject(&:&)
+      end
 
-      elsif key == "subtypes"
-        query =  query.where("'?' = ANY (subtypes)", value)
+      if key == "black" && value == "1"
+        color_query = Color.where(name: "Black").first.cards
+        query = [query, color_query].inject(&:&)
+      end
 
-      else
-        query = query.where(["#{key} iLIKE ?", "%#{value}%"])
+      if key == "red" && value == "1"
+        color_query = Color.where(name: "Red").first.cards
+        query = [query, color_query].inject(&:&)
+      end
+
+      if key == "green" && value == "1"
+        color_query = Color.where(name: "Green").first.cards
+        query = [query, color_query].inject(&:&)
       end
 
     end
+
     query
-    # binding.pry
   end
 
   private
 
   def search_params
-    params.require(:card_fields).permit(:name, :type, :subtypes, :cmc, :cmcmod, :subtypes, :artist)
+    params.require(:card_fields).permit(:name, :cmc, :card_type, :subtypes, :cmcmod, :subtypes, :artist, :red, :black, :blue, :green, :white)
   end
 
 end
+
+
+
+
+
