278a803678ea7aeab83715abe7be40511bb2a0e2
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-15 21:32:47 -0500

    Fixed merge conflict in sessions controller and schema

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
-    @user = login(params[:email], params[:password], params[:remember_me])
-    if @user
-      redirect_back_or_to root_url, :notice => "Logged in!"
-=======
     user = login(params[:email], params[:password])
-      if user
-        redirect_back_or_to root_url, :notice => "Logged in!"
->>>>>>> d74e72a2506ab6b39df588b0462c30b0fbcea94d
+    if user
+      redirect_back_or_to root_url, :notice => "Logged in!"
     else
       flash.now.alert = "Email or password was invalid"
       render :new
