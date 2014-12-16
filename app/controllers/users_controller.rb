5c867dae6fb025263969ca997b787a75bba40a15
Eman <ericjboshart@gmail.com>
2014-12-16 18:09:24 -0500

    fucking around

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ def new
   @user = User.new
 end
 
+def index
+  @bert = User.last 
+  
+end
 def create
     @user = User.new(user_params)
     if @user.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,16 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
-  validates_presence_of :email
-  validates_uniqueness_of :email
+validates_confirmation_of :password
+validates_presence_of :password, :on => :create
+validates_presence_of :email
+validates_uniqueness_of :email
 
-  after_validation :geocode
+reverse_geocoded_by :latitude, :longitude
+after_validation :reverse_geocode
 
-  reverse_geocoded_by :latitude, :longitude 
-  
-  has_many :listed_cards
-  has_many :trades
+has_many :listed_cards
+has_many :trades
 end
 
 
