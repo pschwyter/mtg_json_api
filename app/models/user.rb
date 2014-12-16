7bc517e0cb78bc8c9fe4adb8441392edb3572b2b
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-16 16:03:31 -0500

    Fixed foreign keys associating a trade to a user as initiator or receiver

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@ class User < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude 
   
   has_many :listed_cards
-  has_many :trades
+  has_many :initiated_trades, class_name: 'Trade', :foreign_key => 'initiator_id'
+  has_many :received_trades, class_name: 'Trade', :foreign_key => 'receiver_id'
 end
 
 
