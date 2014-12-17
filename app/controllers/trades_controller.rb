549bd808b40dc4cbd258864b1cf94c8cfb433c3f
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-16 20:02:48 -0500

    Added new, create and index actions to trades controller

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -4,12 +4,24 @@ class TradesController < ApplicationController
 	end
 
 	def index
+		@trades = Trade.where(initiator_id: current_user.id)
 	end
 
 	def new
+		@user = User.find(params[:user_id])
+		@trade = Trade.new
 	end
 
 	def create
+		@trade = Trade.create(receiver_id: params[:user_id], 
+							  start_initiator_list: params[:start_initiator_list],
+							  start_receiver_list: params[:start_receiver_list]
+							  )
+		@trade.initiator = current_user
+		if @trade.save
+			binding.pry
+			redirect_to user_trades_path(current_user.id)
+		end
 	end
 
 	def edit
@@ -21,4 +33,11 @@ class TradesController < ApplicationController
 	def destroy
 	end
 
+	private
+
 end
+
+
+
+
+
