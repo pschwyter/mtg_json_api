41f2ba6404712b7dd7829aa2d4b5e2f6c1778700
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-17 21:04:25 -0500

    Fixed trades controller actions and relevant views to allow for blank trade lists

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -5,19 +5,18 @@ class TradesController < ApplicationController
 	end
 
 	def index
-		@trades = Trade.where(initiator_id: current_user.id)
+		@trades = Trade.where("(initiator_id = ? or receiver_id = ?)", current_user.id, current_user.id)
 	end
+
 
 	def new
 		@user = User.find(params[:user_id])
-		@trade = Trade.new
+		@trade = @user.received_trades.new
 	end
 
 	def create
-		@trade = Trade.create(receiver_id: params[:user_id], 
-							  cards_from_initiator: params[:cards_from_initiator],
-							  cards_from_receiver: params[:cards_from_receiver]
-							  )
+		@user = User.find(params[:user_id])
+		@trade = @user.received_trades.build(trade_params)
 		@trade.initiator = current_user
 		if @trade.save
 			redirect_to user_trades_path(current_user.id)
@@ -25,15 +24,14 @@ class TradesController < ApplicationController
 	end
 
 	def edit
+		Trade.find(params[:id]).save
 		@trade = Trade.find(params[:id])
 		@user = @trade.receiver
 	end
 
 	def update
 		@trade = Trade.find(params[:id])
-		@trade.update(cards_from_initiator: params[:cards_from_initiator],
-							  cards_from_receiver: params[:cards_from_receiver]
-							  )
+		@trade.update(trade_params)
 		if @trade.save
 			redirect_to user_trades_path(current_user.id)
 		end
@@ -44,8 +42,29 @@ class TradesController < ApplicationController
 
 	private
 
-end
+	# During a new trade creation, if list is left blank params[:trade] will be missing one or both card_from arrays (ie. nil)
+	# and that will cause Trade creation to break
+	# Just realize that I should wrap the whole thing in an if statement, but too tired, FIX TOMORROW
+	# (params[:trade].has_key?(:cards_from_initiator) ? cards_from_initiator: params[:trade][:cards_from_initiator] : cards_from_initiator: [] )
+	def raw_trade_params
+		if params.has_key?(:trade)
+		{
+			cards_from_initiator: 	(params[:trade].has_key?(:cards_from_initiator) ? params[:trade][:cards_from_initiator] : [] ),
+			cards_from_receiver: 	(params[:trade].has_key?(:cards_from_receiver) ? params[:trade][:cards_from_receiver] : [])
+		}
+		else
+			params[:trade] = {cards_from_initiator: [], cards_from_receiver: []}
+		end
+	end
 
+	def trade_params
+		Hash[
+			 cards_from_initiator: raw_trade_params.map {|k,v| v.map {|i| i.to_i}}[0], 
+			 cards_from_receiver: raw_trade_params.map {|k,v| v.map {|i| i.to_i}}[1]
+			]
+	end
+
+end
 
 
 
