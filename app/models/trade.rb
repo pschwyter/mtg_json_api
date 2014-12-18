c015d21b4367317bae6754944f38c1ef84b73ba8
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-18 00:16:46 -0500

    Created exchange_cards method which changes the ownership of cards in the trade model

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,7 +3,7 @@ class Trade < ActiveRecord::Base
 	belongs_to :receiver, class_name: 'User'
 
 	before_save :replace_nil_with_array
-	before_save :check_if_complete
+	before_save :update_trade_status
 
 	def accept(user)
 		if self.initiator == user
@@ -23,6 +23,28 @@ class Trade < ActiveRecord::Base
 		end
 	end
 
+	def exchange_cards
+		initiator_cards_to_trade = self.cards_from_initiator.map {|card_id| ListedCard.find(card_id)}
+		receiver_cards_to_trade = self.cards_from_receiver.map {|card_id| ListedCard.find(card_id)} 
+
+		initiator_user = self.initiator
+		receiver_user = self.receiver
+
+		initiator_cards_to_trade.each do |card| 
+			p card.user
+			card.user = self.receiver
+			card.save
+			p card.user
+		end
+		receiver_cards_to_trade.each do |card| 
+			p card.user
+			card.user = self.initiator
+			card.save
+			p card.user
+		end
+		self.save
+	end
+
 	private
 
 	def replace_nil_with_array
@@ -34,9 +56,17 @@ class Trade < ActiveRecord::Base
 		end
 	end
 
-	def check_if_complete
+	def update_trade_status
 		if self.initiator_accepted == true && self.receiver_accepted == true
 			self.status = "complete"
 		end
 	end
+
 end
+
+
+
+
+
+
+
