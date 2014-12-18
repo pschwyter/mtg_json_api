990ff346694a65b7adf4b69ea47639125550fb3f
Philippe Schwyter <pschwyter90@gmail.com>
2014-12-17 21:02:26 -0500

    Added before_save method to ensure a Trade's cards_from arrays are never nil

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,4 +1,18 @@
 class Trade < ActiveRecord::Base
 	belongs_to :initiator, class_name: 'User'
 	belongs_to :receiver, class_name: 'User'
+
+	before_save :replace_nil_with_array
+
+
+	private
+
+	def replace_nil_with_array
+		if self.cards_from_initiator == nil
+			self.cards_from_initiator = []
+		end
+		if self.cards_from_receiver == nil
+			self.cards_from_receiver = []
+		end
+	end
 end
