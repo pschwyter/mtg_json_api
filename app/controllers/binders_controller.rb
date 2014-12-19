7329976d36f9cb74258059114fe413cfa245a65a
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-12-18 22:21:42 -0500

    pulling phils updates to list models for geo binders

--- app/controllers/binders_controller.rb
+++ app/controllers/binders_controller.rb
@@ -6,8 +6,23 @@ def index
 end
 
 def show
-@card = Card.find(params[:id])
-@users = User.all
+@cards = Card.find(params[:id])
+@users = match_users
+
+end
+
+private
+
+def match_users
+	listed_cards = ListedCard.where(card_id: (params[:id])) 
+	result = []
+	listed_cards.each {|listed_card| result << listed_card unless listed_card.tradeable_list_id == nil}
+	users = []
+	result.each {|card| users << card.tradeable_list.user}
+
+	users = User.where(id: users.map(&:id))
+
+	users
 end
 
 
