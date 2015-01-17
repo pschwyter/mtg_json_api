a3865627ecf4521a4aeca5f2cb99eb4c130a5b7c
Philippe Schwyter <pschwyter90@gmail.com>
2015-01-16 22:53:29 -0500

    Fixed indentation and closing tags

--- app/assets/javascripts/fullscreener.js
+++ app/assets/javascripts/fullscreener.js
@@ -1,14 +1,13 @@
-function fullscreener() {
-	var total = $(document).height()-222;
-	if ($(".section-inner").height() != total) {
-		$(".section-inner").css({"height":total*2})
-}
-}
+$( document ).ready(function() {
+	        console.log( "document loaded" );
 
-
-$(document).on('ready page:load', function() {
-
-fullscreener()
+	// fullscreener();
 })
 
 
+function fullscreener() {
+	var total = $(document).height()-222;
+	if ($(".section-inner").height() != total) {
+		$(".section-inner").css({"height":total})
+	}
+}
