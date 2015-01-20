7eb290e566465e81ddf21d589df839b52931b369
Nurbrun <bruno.alfano.rizzo@gmail.com>
2015-01-20 17:08:57 -0500

    reworked the modal before presentation

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -17,4 +17,21 @@ module CardsHelper
 		"http://mtgimage.com/symbol/mana/#{type}/16.gif"
 	end
 
+	def all_mana_cost_images_large(card)
+		if card.mana_cost
+
+			tags = ""
+			
+			card.mana_cost.scan(/\w/).each do |mc|
+				tags += image_tag mana_cost_url_large(mc)
+			end
+			tags.html_safe
+		
+		end
+	end
+
+	def mana_cost_url_large(type)
+		"http://mtgimage.com/symbol/mana/#{type}/32.gif"
+	end
+
 end
