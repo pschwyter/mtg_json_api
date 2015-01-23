a6b4274d21653f4adb23f4b6b5e6cda9263f4927
Philippe Schwyter <pschwyter90@gmail.com>
2015-01-23 01:11:37 -0500

    Moved comment submit form into comment div

--- app/assets/javascripts/sliding_menu.js
+++ app/assets/javascripts/sliding_menu.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	var sliding_menu_height = $(window).height();
+
 	$(window).resize(function(){
 		var sliding_menu_height = $(window).height();
 		$('.sliding-menu-content').height(sliding_menu_height);
